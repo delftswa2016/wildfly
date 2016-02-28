60a603e87d0357c5bc59629a02386120c23ce0fe
ileontiuc <ileontiuc@yahoo.com>
2016-02-28 01:28:29 +0100

    Revert "Updated undertowSubsystemTestcase to remove code duplication"

--- undertow/src/test/java/org/wildfly/extension/undertow/UndertowSubsystem10TestCase.java
+++ undertow/src/test/java/org/wildfly/extension/undertow/UndertowSubsystem10TestCase.java
@@ -46,7 +46,7 @@ import org.wildfly.extension.undertow.filters.FilterService;
  *
  * @author <a href="mailto:tomaz.cerar@redhat.com">Tomaz Cerar</a>
  */
-public class UndertowSubsystem10TestCase extends UndertowSubsystemTest {
+public class UndertowSubsystem10TestCase extends AbstractSubsystemBaseTest {
 
     public UndertowSubsystem10TestCase() {
         super(UndertowExtension.SUBSYSTEM_NAME, new UndertowExtension());
@@ -76,7 +76,65 @@ public class UndertowSubsystem10TestCase extends UndertowSubsystemTest {
         return super.standardSubsystemTest(configId, false);
     }
 
-     @Override
+    @Test
+    public void testRuntime() throws Exception {
+        System.setProperty("server.data.dir", System.getProperty("java.io.tmpdir"));
+        System.setProperty("jboss.home.dir", System.getProperty("java.io.tmpdir"));
+        System.setProperty("jboss.home.dir", System.getProperty("java.io.tmpdir"));
+        System.setProperty("jboss.server.server.dir", System.getProperty("java.io.tmpdir"));
+        KernelServicesBuilder builder = createKernelServicesBuilder(UndertowSubsystemTestCase.RUNTIME)
+                .setSubsystemXml(getSubsystemXml());
+        KernelServices mainServices = builder.build();
+        if (!mainServices.isSuccessfulBoot()) {
+            Assert.fail(mainServices.getBootError().toString());
+        }
+        ServiceController<FilterService> connectionLimiter = (ServiceController<FilterService>) mainServices.getContainer().getService(UndertowService.FILTER.append("limit-connections"));
+        connectionLimiter.setMode(ServiceController.Mode.ACTIVE);
+        FilterService connectionLimiterService = connectionLimiter.getService().getValue();
+        HttpHandler result = connectionLimiterService.createHttpHandler(Predicates.truePredicate(), new PathHandler());
+        Assert.assertNotNull("handler should have been created", result);
+
+
+        ServiceController<FilterService> headersFilter = (ServiceController<FilterService>) mainServices.getContainer().getService(UndertowService.FILTER.append("headers"));
+        headersFilter.setMode(ServiceController.Mode.ACTIVE);
+        FilterService headersService = headersFilter.getService().getValue();
+        HttpHandler headerHandler = headersService.createHttpHandler(Predicates.truePredicate(), new PathHandler());
+        Assert.assertNotNull("handler should have been created", headerHandler);
+
+        final ServiceName hostServiceName = UndertowService.virtualHostName("default-server", "other-host");
+        ServiceController<Host> hostSC = (ServiceController<Host>) mainServices.getContainer().getService(hostServiceName);
+        Assert.assertNotNull(hostSC);
+        hostSC.setMode(ServiceController.Mode.ACTIVE);
+        Host host = hostSC.getValue();
+        Assert.assertEquals(1, host.getFilters().size());
+
+
+        final ServiceName locationServiceName = UndertowService.locationServiceName("default-server", "default-host", "/");
+        ServiceController<LocationService> locationSC = (ServiceController<LocationService>) mainServices.getContainer().getService(locationServiceName);
+        Assert.assertNotNull(locationSC);
+        locationSC.setMode(ServiceController.Mode.ACTIVE);
+        LocationService locationService = locationSC.getValue();
+        Assert.assertNotNull(locationService);
+        connectionLimiter.setMode(ServiceController.Mode.REMOVE);
+        final ServiceName jspServiceName = UndertowService.SERVLET_CONTAINER.append("myContainer");
+        ServiceController<ServletContainerService> jspServiceServiceController = (ServiceController<ServletContainerService>) mainServices.getContainer().getService(jspServiceName);
+        Assert.assertNotNull(jspServiceServiceController);
+        JSPConfig jspConfig = jspServiceServiceController.getService().getValue().getJspConfig();
+        Assert.assertNotNull(jspConfig);
+        Assert.assertNotNull(jspConfig.createJSPServletInfo());
+
+        final ServiceName filterRefName = UndertowService.filterRefName("default-server", "other-host", "/", "static-gzip");
+
+        ServiceController<FilterRef> gzipFilterController = (ServiceController<FilterRef>) mainServices.getContainer().getService(filterRefName);
+        gzipFilterController.setMode(ServiceController.Mode.ACTIVE);
+        FilterRef gzipFilterRef = gzipFilterController.getService().getValue();
+        HttpHandler gzipHandler = gzipFilterRef.createHttpHandler(new PathHandler());
+        Assert.assertNotNull("handler should have been created", gzipHandler);
+
+
+    }
+
+    @Override
     protected AdditionalInitialization createAdditionalInitialization() {
         return UndertowSubsystemTestCase.DEFAULT;
     }
